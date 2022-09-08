cb0ba71defd2204ea3dfc0dfd88e6b5c1d05d8e1
Vit111 <busuevaulia692@gmail.com>
2022-09-08 10:49:36 +0300

    first commit

--- main.js
+++ main.js
@@ -1,32 +1,40 @@
 $('.submit').on('click', function (event) {
     event.preventDefault()
+    $('.text').empty();
+    $('.inner').empty();
+    $('.lead').empty();
+    let input = $('input').val()
+    console.log(input)
     let Selectvalue = $('select').val()
     $.ajax({
-        url: `https://swapi.dev/api/${Selectvalue}`,
+        url: `https://swapi.dev/api/${Selectvalue}?search=${input}`,
         method: 'GET',
         success: function (result) {
             let items = [];
             let item = []
             let list = []
             $.each(result, function (key, data) {
-                console.log(data)
                 if (key != 'results') {
                     return
                 }
                 else {
                     items.push("<h2>" + Selectvalue + ':' + "</h2>")
                     $.each(data, function (index, val) {
+                        console.log(val)
                         $.each(val, function (el, part) {
-                            if (el === 'name') {
+                            if (el === 'name' || el === 'title') {
                                 item.push(`<li> ${el} : <span class='name'>${part}</span></li>`)
                             }
                         })
                     })
                 }
             })
+            console.log(item)
             $('.text').append(item)
             $('.inner').append(items)
             $('.name').on('click', function (event) {
+                $('.lead').empty();
+                $('.name').removeClass('unique');
                 event.preventDefault()
                 $(this).addClass('unique');
                 let inputValue = $(this).text()
@@ -51,8 +59,9 @@ $('.submit').on('click', function (event) {
                                 })
                             }
                         })
+                        $('.lead').prepend(`<button type="button" name="submit" class="delete">Delete</button>`).delay(1000).show(2000);
                         $('.lead').append(list).delay(1000).show(2000);
-                        $('.lead').on('click', function (event) {
+                        $('.delete').on('click', function (event) {
                             event.preventDefault()
                             $('.lead').empty();
                             $('.name').removeClass('unique');
@@ -64,6 +73,83 @@ $('.submit').on('click', function (event) {
         }
     })
 })
+
+
+
+
+
+// $('.submit').on('click', function (event) {
+//     event.preventDefault()
+//     $('.text').empty();
+//     $('.inner').empty();
+//     $('.lead').empty();
+//     let Selectvalue = $('select').val()
+//     $.ajax({
+//         url: `https://swapi.dev/api/${Selectvalue}`,
+//         method: 'GET',
+//         success: function (result) {
+//             let items = [];
+//             let item = []
+//             let list = []
+//             $.each(result, function (key, data) {
+//                 if (key != 'results') {
+//                     return
+//                 }
+//                 else {
+//                     items.push("<h2>" + Selectvalue + ':' + "</h2>")
+//                     $.each(data, function (index, val) {
+//                         console.log(val)
+//                         $.each(val, function (el, part) {
+//                             if (el === 'name' || el === 'title') {
+//                                 item.push(`<li> ${el} : <span class='name'>${part}</span></li>`)
+//                             }
+//                         })
+//                     })
+//                 }
+//             })
+//             console.log(item)
+//             $('.text').append(item)
+//             $('.inner').append(items)
+//             $('.name').on('click', function (event) {
+//                 $('.lead').empty();
+//                 $('.name').removeClass('unique');
+//                 event.preventDefault()
+//                 $(this).addClass('unique');
+//                 let inputValue = $(this).text()
+//                 console.log(inputValue)
+//                 $.ajax({
+//                     url: `https://swapi.dev/api/${Selectvalue}/?search=${inputValue}`,
+//                     method: 'GET',
+//                     success: function (final) {
+//                         console.log(final)
+//                         let list = []
+//                         $.each(final, function (key, data) {
+//                             console.log(data)
+//                             if (key != 'results') {
+//                                 return
+//                             }
+//                             else {
+//                                 $.each(data, function (index, val) {
+//                                     $.each(val, function (el, part) {
+//                                         list.push(`<li> ${el} : <span class='info'>${part}</span></li>`)
+//                                     }
+//                                     )
+//                                 })
+//                             }
+//                         })
+//                         $('.lead').append(list).delay(1000).show(2000);
+//                         $('.lead').on('click', function (event) {
+//                             event.preventDefault()
+//                             $('.lead').empty();
+//                             $('.name').removeClass('unique');
+//                         })
+//                     }
+//                 })
+//             }
+//             )
+//         }
+//     })
+// })
 
 
 
